2a45cd0472f628f4c5fc42182fcd4990010cc023
peterkwesiansah <ansahpeter123@yahoo.com>
2021-07-06 08:24:23 +0000

    baller

--- src/App.js
+++ src/App.js
@@ -9,99 +9,115 @@ import {
 	FormLabel,
 	Button,
 } from '@material-ui/core';
-const App = () => {
-	const [stage, setStage] = useState(0);
-	const [value, setValue] = useState('');
-	const [userData, setData] = useState([]);
-	const handleChange = (event) => {
-		setValue(event.target.value);
-	};
-	const options = [
-		{ value: 0, label: 'No' },
-		{ value: 1, label: 'Yes' },
-	];
-	//dollar man
+const options = [
+	{ value: 0, label: 'No' },
+	{ value: 1, label: 'Yes' },
+];
+//dollar man
 
-	const questions = {
-		general: [
-			{
-				question:
-					'Have you travelled out of the borders of Ghana in the past 14 days',
-				options,
-			},
-			{
-				question:
-					'Have you within the past 14 days travelled in an area with known community spread of CODVID-19?',
-				options,
-			},
-			{
-				question:
-					'Have you come in contact with someone,who has recently travelled outside Ghana within the past 14 days?',
-				options,
-			},
-			{
-				question:
-					'Have you been in contact with a confirmed case of COVID-19 in the last 14 days',
-				options,
-			},
-			// [
-			// 	{ value: 0, label: 'No' },
-			// 	{ value: 1, label: 'Yes' },
-			// ],
-		],
-		symptoms: [
-			{ question: 'Fever', options },
-			{ question: 'Tiredness', options },
-			{ question: 'Cough', options },
-			{ question: 'Headache', options },
-			{ question: 'Sneezing', options },
-			{ question: 'Sore Throat', options },
-			{ question: 'Runny Nose', options },
-			{ question: 'Difficulty in Breathing and shortness of breath', options },
-			{ question: 'Recent loss of smell  and taste', options },
-			{
-				question: 'Persistenet pain or pressure in the chest',
-				options,
-			},
-		],
+const questions = {
+	general: [
+		{
+			question:
+				'Have you travelled out of the borders of Ghana in the past 14 days',
+			options,
+		},
+		{
+			question:
+				'Have you within the past 14 days travelled in an area with known community spread of CODVID-19?',
+			options,
+		},
+		{
+			question:
+				'Have you come in contact with someone,who has recently travelled outside Ghana within the past 14 days?',
+			options,
+		},
+		{
+			question:
+				'Have you been in contact with a confirmed case of COVID-19 in the last 14 days',
+			options,
+		},
+		// [
+		// 	{ value: 0, label: 'No' },
+		// 	{ value: 1, label: 'Yes' },
+		// ],
+	],
+	symptoms: [
+		{ question: 'Fever', options },
+		{ question: 'Tiredness', options },
+		{ question: 'Cough', options },
+		{ question: 'Headache', options },
+		{ question: 'Sneezing', options },
+		{ question: 'Sore Throat', options },
+		{ question: 'Runny Nose', options },
+		{ question: 'Difficulty in Breathing and shortness of breath', options },
+		{ question: 'Recent loss of smell  and taste', options },
+		{
+			question: 'Persistenet pain or pressure in the chest',
+			options,
+		},
+	],
+};
+
+const App = () => {
+	const [qIndex, setqIndex] = useState(0);
+	const [qSet, setqSet] = useState('general');
+	const question = questions[qSet][qIndex];
+
+	const [answers, setAnswers] = useState({
+		general: {},
+		symptoms: {},
+	});
+	const handleChange = (event) => {
+		const state = { ...answers };
+		state[qSet][qIndex] = event.target.value;
+		setAnswers(state);
+	};
+	console.log(answers);
+	const nextQuestion = () => {
+		setqIndex(qIndex + 1);
+		if (qSet === options[qSet].length - 1) {
+			setqSet('symptoms');
+			setqIndex(0);
+		}
+	};
+	const previousQuestion = () => {
+		setqIndex(qIndex - 1);
 	};
 
 	return (
 		<div className="App">
-			<h3 style={{ textTransform: 'capitalize' }}>
-				{Object.keys(questions)[0]}
-			</h3>
-
-			<p>{questions.general[stage].question}</p>
+			<div className="appNav">
+				<h1>General</h1>
+				<h1>symptoms</h1>
+			</div>
 			<FormControl component="fieldset">
-				<FormLabel
-					component="legend"
-					style={{ flexDirection: 'row', display: 'flex' }}
+				<FormLabel component="legend">{question.question}</FormLabel>
+				<RadioGroup
+					aria-label={qSet}
+					name={qSet}
+					value={'Yes'}
+					onChange={handleChange}
 				>
-					{options.map((item, index) => (
-						<RadioGroup
-							aria-label="gender"
-							name="general"
-							value={value}
-							onChange={handleChange}
-						>
-							<FormControlLabel
-								value={item.label}
-								control={<Radio color="primary" />}
-								label={item.label}
-							/>
-						</RadioGroup>
-					))}
-				</FormLabel>
+					<FormControlLabel
+						value={question.options[1].value}
+						control={<Radio />}
+						label={question.options[1].label}
+					/>
+					<FormControlLabel
+						value={question.options[0].value}
+						control={<Radio />}
+						label={question.options[0].label}
+					/>
+				</RadioGroup>
 			</FormControl>
 			<div>
 				<Button
 					variant="contained"
 					color="primary"
 					style={{ marginRight: '50%' }}
-					onClick={() =>
-						stage > 0 ? setStage(stage - 1) : console.log('vibes')
-					}
+					onClick={previousQuestion}
+					disabled={qIndex === 0}
 				>
 					back
 				</Button>
@@ -109,11 +125,8 @@ const App = () => {
 				<Button
 					variant="contained"
 					color="primary"
-					onClick={() =>
-						stage < questions.general.length - 1
-							? setStage(stage + 1)
-							: console.log('vibes')
-					}
+					onClick={nextQuestion}
+					disabled={!answers[qSet][qIndex]}
 				>
 					next
 				</Button>
