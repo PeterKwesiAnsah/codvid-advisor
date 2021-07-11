1aa9db6560d179830bb8f6d5640967677e4d3c5d
peterkwesiansah <ansahpeter123@yahoo.com>
2021-07-11 14:13:40 +0000

    latest

--- src/ProgressCalculater.js
+++ src/ProgressCalculater.js
@@ -1,5 +1,5 @@
 export const progressCalculater = (currentIndex, totalLength) => {
 	//totalLength = totalLength - 1;
-	console.log(totalLength);
-	return (currentIndex / totalLength) * 100;
+	// console.log(totalLength);
+	return (currentIndex+1 / totalLength) * 100;
 };
